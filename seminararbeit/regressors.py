f8eeaaf2a0d2b80104f43d3dbea5778ec2375e75
MaxUhl98 <frozen2ice@web.de>
2024-09-25 12:42:52 +0200

    added submission creation to skripts

--- seminararbeit/regressors.py
+++ seminararbeit/regressors.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
@@ -5,24 +7,30 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import root_mean_squared_log_error
 from xgboost import XGBRegressor
 from openfe import OpenFE, transform
+from utils.helpers import create_submission
 
 if __name__ == '__main__':
+    os.chdir('..')
     # Model initialization keyword arguments
     model_kwargs = {XGBRegressor: {'objective': 'reg:squaredlogerror'}, LinearRegression: {}}
     n_jobs: int = 4  # Number of CPU Cores used for Feature Engineering
     use_openfe: bool = False  # Decide to use openfe
-    model = XGBRegressor  # XGBRegressor to perform Gradient Boosting, LinearRegression to perform Linear Regression
-    folder = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)  # Initialize Splitter for Cross Validation
+    model = LinearRegression # XGBRegressor to perform Gradient Boosting, LinearRegression to perform Linear Regression
+    num_folds:int = 10
+    folder = StratifiedKFold(n_splits=num_folds, random_state=1, shuffle=True)  # Initialize Splitter for Cross Validation
 
     X = pd.read_csv('playground-series-s4e4/train.csv').drop(columns='id')  # X:= Features, y:= Target
     y = X.pop('Rings')
+    X_test = pd.read_csv('playground-series-s4e4/test.csv').drop(columns='id')  # Load Test Data
     if use_openfe:
         # Preprocess Features with openfe
         features = OpenFE().fit(X, y, task='regression', n_jobs=n_jobs) # Select useful candidate features
-        X, _ = transform(X, X.iloc[0:0], features, n_jobs=n_jobs)  # Transform dataset using openfe generated features
+        X, X_test = transform(X, X_test, features, n_jobs=n_jobs)  # Transform dataset using openfe generated features
     X = pd.get_dummies(X)  # One hot encoding
+    X_test = pd.get_dummies(X_test)
     splits = folder.split(X, y)  # Create Cross Validation Splits
     fold_scores = []
+    trained_models = []
 
     for train, test in splits:
         # Select train and test data according to CV indices
@@ -31,10 +39,11 @@ if __name__ == '__main__':
 
         if model == LinearRegression:
             # Log transform target to help the regression out (regression always uses MSE)
-            y_train, y_val = np.log1p(y_train), np.log1p(y_val)
+            y_train, y_val = np.log1p(y_train), y_val
 
         current_model = model(**model_kwargs[model])  # Initialize Model with keyword arguments from line 11
         current_model.fit(X_train, y_train)  # Train model
+        trained_models.append(current_model) # Add current model to trained model list
 
         # Get predictions
         pred = current_model.predict(X_val) if model == XGBRegressor else np.expm1(current_model.predict(X_val))
@@ -43,3 +52,19 @@ if __name__ == '__main__':
         fold_scores.append(fold_rmsle)
 
     print(f'Val RMSLE: {np.mean(fold_scores)}')  # Print mean RMSLE across all folds
+
+    if model == LinearRegression:
+        model_predictions = [np.expm1(_model.predict(X_test.values)) for _model in trained_models]
+    else:
+        model_predictions = [_model.predict(X_test.values) for _model in trained_models]
+
+    model_predictions = sum(model_predictions) / num_folds
+    submission_dataframe = pd.read_csv('playground-series-s4e4/sample_submission.csv')  # Load sample submission
+    submission_dataframe['Rings'] = model_predictions  # Set autogluon submissions as predictions
+    submission_save_directory = f'seminararbeit/submissions/{model.__name__}/submission.csv'
+    try:
+        submission_dataframe.to_csv(submission_save_directory,index=False)  # Save submission csv
+    except OSError:
+        os.mkdir(submission_save_directory)
+        submission_dataframe.to_csv(submission_save_directory,index=False)  # Save submission csv
+
--- seminararbeit/seminar_code_autogluon.py
+++ seminararbeit/seminar_code_autogluon.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from autogluon.tabular import TabularDataset, TabularPredictor
 from autogluon.core.metrics import make_scorer
@@ -5,19 +6,33 @@ from sklearn.metrics import root_mean_squared_log_error
 from openfe import OpenFE, transform
 
 if __name__ == '__main__':
-    data_path: str = 'playground-series-s4e4/train.csv'  # Path to data csv file
+    os.chdir('..')
+    train_data_path: str = 'playground-series-s4e4/train.csv'  # Path to train data csv file
+    test_data_path = 'playground-series-s4e4/test.csv' # Path to test data csv file
+    submission_save_directory = 'seminararbeit/submissions/autogluon/submission.csv' # directory in which the submission will get saved
     n_jobs: int = 4  # Number of used CPU cores for feature engineering
     use_openfe: bool = True  # Decide to use openfe
-    data = TabularDataset(pd.read_csv(data_path)).drop(columns='id')  # Load Data
+    data = TabularDataset(pd.read_csv(train_data_path)).drop(columns='id')  # Load Training Data
+    X_test = TabularDataset(pd.read_csv(test_data_path)).drop(columns='id')  # Load Test Data
     if use_openfe:
         X = data
         y = data.pop('Rings')  # Split dataset into features X and target y
         features = OpenFE().fit(X, y, task='regression', n_jobs=n_jobs)  # Select useful features
-        X, _ = transform(X, X.iloc[0:0], features, n_jobs=n_jobs)  # Transform original data
+        X, X_test = transform(X, X_test, features, n_jobs=n_jobs)  # Transform original data
         data = pd.concat([X, y], axis=1)  # Concatenate data to one dataset
+
 
     # Initialize Autogluon Tabular Regressor
     model = TabularPredictor('Rings', eval_metric=make_scorer('rmsle', root_mean_squared_log_error,
                                                               greater_is_better=False), problem_type='regression')
     model.fit(data, presets='best_quality')  # Fit regressor to data
     print(model.fit_summary()['leaderboard'].to_string())  # Print training summary
+
+    predictions = model.predict(X_test) # Make predictions on test data
+    submission_dataframe = pd.read_csv('playground-series-s4e4/sample_submission.csv') # Load sample submission
+    submission_dataframe['Rings'] = predictions # Set autogluon submissions as predictions
+    try:
+        submission_dataframe.to_csv(submission_save_directory,index=False)  # Save submission csv
+    except OSError:
+        os.mkdir(submission_save_directory)
+        submission_dataframe.to_csv(submission_save_directory,index=False)  # Save submission csv
